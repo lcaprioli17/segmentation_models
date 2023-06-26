fdf5daa15b36f51c299ffe39f29287933951d199
lcaprioli17 <93045436+lcaprioli17@users.noreply.github.com>
2023-06-26 14:10:40 +0200

    Update _common_blocks.py

--- segmentation_models/models/_common_blocks.py
+++ segmentation_models/models/_common_blocks.py
@@ -58,10 +58,8 @@ def Conv2dBn(
             name=conv_name,
         )(input_tensor)
 
-        # if filters < 128:
-        #     x = layers.Dropout(0.3)(x)
-        # else:
-        #     x = layers.Dropout(0.5)(x)
+        x = layers.Dropout(0.3)(x)
+
 
         if use_batchnorm:
             x = layers.BatchNormalization(axis=bn_axis, name=bn_name)(x)
