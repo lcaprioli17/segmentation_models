4c4f625a59e5ee2ec1ed7be802bfb81b63bc7df0
lcaprioli17 <93045436+lcaprioli17@users.noreply.github.com>
2023-06-27 12:35:13 +0200

    Update unet.py

--- segmentation_models/models/unet.py
+++ segmentation_models/models/unet.py
@@ -119,6 +119,8 @@ def build_unet(
     input_ = backbone.input
     x = backbone.output
 
+    x = Dropout(0.5)(x)
+    
     # extract skip connections
     skips = ([backbone.get_layer(name=i).output if isinstance(i, str)
               else backbone.get_layer(index=i).output for i in skip_connection_layers])
